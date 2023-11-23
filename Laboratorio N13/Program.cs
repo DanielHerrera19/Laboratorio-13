8a2b1967b4e003a8df74232a285a1add0971ddf4
pc <pc@LAPTOP-JLMPM40M>
2023-11-23 18:00:12 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio N13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Laboratorio_N13.Lab13;
+
+namespace Laboratorio_N13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = InterfazConsola.MenuPrincipal();
+
+            do
+            {
+                Console.Clear();
+                switch (opcion)
+                {
+                    case 1:
+                        opcion = InterfazConsola.RealizarEncuesta();
+                        break;
+                    case 2:
+                        opcion = InterfazConsola.DatosRegistrados();
+                        break;
+                    case 3:
+                        opcion = InterfazConsola.EliminarDato();
+                        break;
+                    case 4:
+                        opcion = InterfazConsola.OrdenarDatos();
+                        break;
+                    case 0:
+                    default:
+                        opcion = InterfazConsola.MenuPrincipal();
+                        break;
+                }
+            } while (opcion != 5);
+        }
+    }
+}
